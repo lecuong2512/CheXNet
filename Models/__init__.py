8e2584b421a64506eaa5061efc4ec248c0ef79c9
lecuong2512 <92969758+lecuong2512@users.noreply.github.com>
2025-10-25 02:28:18 +0700

    c
    Added module documentation and versioning.

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,3 +1,22 @@
-from .Model import DenseNet121, ConvNeXtV2Large
+"""
+CheXNet Models Package
+Multi-architecture support for chest X-ray classification
+"""
+
+from .Model import (
+    MultiModelArchitecture,
+    DenseNet121,
+    ConvNeXtV2Large
+)
 from .read_data import DatasetGenerator
 from .TrainModel import ChexnetTrainer
+
+__all__ = [
+    'MultiModelArchitecture',
+    'DenseNet121',
+    'ConvNeXtV2Large',
+    'DatasetGenerator',
+    'ChexnetTrainer'
+]
+
+__version__ = '2.0.0'
